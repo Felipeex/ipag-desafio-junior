ce1935131ef299f250f105d41073860ce45728a3
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2024-03-27 23:57:42 -0300

    feat(basic-calculator): implement division, multiplication...

--- basic-calculator/src/script/operations.js
+++ basic-calculator/src/script/operations.js
@@ -3,6 +3,7 @@ const operationsInputs = document.getElementsByClassName("operation-button");
 let sum = 0;
 let lastOperation = null;
 let lastOperationUntilEqual = null;
+let isNotFirstOperation = false;
 
 for (const button of operationsInputs) {
   handleButtonFocusedWithClick(button);
@@ -19,53 +20,51 @@ export function removeOperator() {
 
   lastOperation = null;
   lastOperationUntilEqual = null;
-  sum = 0;
+  isNotFirstOperation = false;
 }
 
 function setButtonFocused(button) {
   handleLastOperation(button);
-  handleSum(button.classList[1], Number(numberInput.value));
-  numberInput.value = "";
 
-  if (button.classList[1] === "equal") numberInput.value = sum;
+  if (button.classList[1] !== "equal") {
+    lastOperationUntilEqual = button.classList[1];
+    if (!isNotFirstOperation) {
+      sum += Number(numberInput.value);
+    }
+
+    numberInput.value = "";
+    isNotFirstOperation = true;
+  } else {
+    switch (lastOperationUntilEqual) {
+      case "addition":
+        sum += Number(numberInput.value);
+        break;
+      case "subtraction":
+        sum -= Number(numberInput.value);
+        break;
+      case "division":
+        sum /= Number(numberInput.value);
+        break;
+      case "multiplication":
+        sum *= Number(numberInput.value);
+        break;
+
+      default:
+        break;
+    }
+
+    numberInput.value = sum;
+  }
 }
 
 function handleButtonFocusedWithClick(button) {
   button.addEventListener("click", () => {
     handleLastOperation(button);
-    handleSum(button.classList[1], Number(numberInput.value));
-    numberInput.value = "";
-
-    if (button.classList[1] === "equal") numberInput.value = sum;
   });
 }
-
-function handleSum(operation, value) {
-  switch (operation) {
-    case "addition":
-      sum += value;
-      break;
-    case "equal":
-      handleSum(
-        lastOperationUntilEqual && lastOperationUntilEqual.classList[1],
-        value
-      );
-      break;
-  }
-}
-
 function handleLastOperation(button) {
   if (lastOperation && lastOperation.className.includes("focused"))
     lastOperation.classList.remove("focused");
-
-  if (
-    button &&
-    button.classList[1] === "equal" &&
-    lastOperation &&
-    lastOperation.classList[1] !== "equal"
-  ) {
-    lastOperationUntilEqual = lastOperation;
-  }
 
   lastOperation = button;
   lastOperation.classList.add("focused");
