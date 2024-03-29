b6d1a4e808f82d2ffda133b725edefbcabd31c57
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2024-03-29 03:43:58 -0300

    feat(statistics): create second statistic.

--- 8-statistics/index.js
+++ 8-statistics/index.js
@@ -8,21 +8,38 @@ const prompt = readline.createInterface({
 const salesFiles = readFileSync(`${__dirname}/sales.csv`, {
   encoding: "utf-8",
 });
-
+/* Separando itens na quebra de linha, para ficar me formato de coluna */
 const splitInColumns = salesFiles.split("\r\n");
 
+const regions = [];
 const types = [];
 const sales = [];
+
+/* Percorrendo todas as colunas */
 for (const colunm of splitInColumns) {
+  /* Separando tabelas da coluna */
   const splitInInformations = colunm.split(",");
+  /* Pegando cada informação pelo indice */
+  const region = splitInInformations[0];
   const itemType = splitInInformations[2];
   const unitsSold = splitInInformations[8];
   const totalRevenue = splitInInformations[11];
   const totalCost = splitInInformations[12];
   const totalProfit = splitInInformations[13];
 
+  /* Anotando todas regiões sem repetir */
+  if (!regions.find((otherRegion) => otherRegion === region))
+    regions.push(region);
+
   types.push(itemType);
-  sales.push({ itemType, unitsSold, totalRevenue, totalCost, totalProfit });
+  sales.push({
+    region,
+    itemType,
+    unitsSold,
+    totalRevenue,
+    totalCost,
+    totalProfit,
+  });
 }
 
 function InformationPerTypeOfProducts() {
@@ -47,6 +64,33 @@ function InformationPerTypeOfProducts() {
   }
 }
 
+function SalesPerTypeOfProductsAndRegion() {
+  for (const region of regions) {
+    const findSalesInRegion = sales.filter((sale) => sale.region === region);
+
+    const typesInRegion = [];
+    for (const sale of findSalesInRegion) {
+      if (!typesInRegion.find((type) => type === sale.itemType))
+        typesInRegion.push(sale.itemType);
+    }
+
+    console.log(`\n${region}`);
+    for (const type of typesInRegion) {
+      const findSalesPerTypeOnRegion = findSalesInRegion.filter(
+        (sale) => sale.itemType === type
+      );
+
+      let TotalUnitsSoldPerRegion = 0;
+
+      for (const salePerType of findSalesPerTypeOnRegion) {
+        TotalUnitsSoldPerRegion += Number(salePerType.unitsSold);
+      }
+
+      console.log(`- ${type} = ${TotalUnitsSoldPerRegion} Vendas`);
+    }
+  }
+}
+
 (function main() {
   prompt.question(
     `O que você deseja calcular? \n 1 - Vendas (total de unidades vendidas, total de receita, custo total, lucro total) por tipo de produto. \n 2 - Vendas por tipo de produto e região. \n 3 - Tipo de produto com maior receita de cada país.\n`,
@@ -56,6 +100,7 @@ function InformationPerTypeOfProducts() {
           InformationPerTypeOfProducts();
           break;
         case 2:
+          SalesPerTypeOfProductsAndRegion();
           break;
         case 3:
           break;
