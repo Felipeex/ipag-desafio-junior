196320d939db3164d3a119922d4c77776118f4ce
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2024-03-29 00:11:00 -0300

    feat(todo): delete with button icon

--- 6-todo-list/scripts/index.js
+++ 6-todo-list/scripts/index.js
@@ -5,8 +5,8 @@ import { EditTaskUseCase } from "./use-case/edit-task-use-case.js";
 import { fetchManyTasksUseCase } from "./use-case/fetch-many-tasks.js";
 import { getTaskUseCase } from "./use-case/get-task-use-case.js";
 
-const addTaskButton = document.getElementsByClassName("add-task")[0];
-const taskList = document.getElementsByClassName("tasks")[0];
+const addTaskButton = document.querySelector(".add-task");
+const taskList = document.querySelector(".tasks");
 
 (function renderTasks() {
   const tasks = new fetchManyTasksUseCase(TaksLocalStorage).execute();
@@ -30,15 +30,39 @@ function mountTask({ taskId, status = "pendente", description = "" }) {
   taskList.insertAdjacentHTML(
     "beforeBegin",
     `
-    <div class="task" draggable="true" id="${taskId}">
+    <div class="task" id="${taskId}">
       <input id="checkbox-${taskId}" type="checkbox" ${
       status !== "pendente" && "checked"
     }/>
       <input id="text-${taskId}" type="text" placeholder="Digite uma tarefa..." value="${description}" />
+
+      <iconify-icon class="material-symbols:delete" taskId="${taskId}" icon="material-symbols:delete" width="16" height="16" style="color: #FFF; cursor: pointer;"></iconify-icon>
     </div>
     `
   );
+
+  const deleteElements = document.getElementsByClassName(
+    "material-symbols:delete"
+  );
+  for (const deleteElement of deleteElements) {
+    deleteElement.onclick = () => {
+      const taskId = Number(deleteElement.getAttribute("taskId"));
+      deleteTask(taskId);
+
+      const taskElement = document.getElementById(taskId);
+      taskElement.remove();
+    };
+  }
+
+  const textInput = document.getElementById(`text-${taskId}`);
+  textInput.style.width = `${textInput.value.length * 9}px`;
+
   watchedEdit(taskId);
+}
+
+function deleteTask(taskId) {
+  const deleteTaskUseCase = new DeleteTaskUseCase(TaksLocalStorage);
+  deleteTaskUseCase.execute(taskId);
 }
 
 function createNewTask() {
@@ -60,8 +84,7 @@ function createNewTask() {
       const taskElement = document.getElementById(taskId);
       taskElement.remove();
 
-      const deleteTaskUseCase = new DeleteTaskUseCase(TaksLocalStorage);
-      deleteTaskUseCase.execute(taskId);
+      deleteTask(taskId);
     }
   });
 }
@@ -70,6 +93,10 @@ function watchedEdit(taskId) {
   const input = document.getElementById(`text-${taskId}`);
   const checkbox = document.getElementById(`checkbox-${taskId}`);
   const editTaskUseCase = new EditTaskUseCase(TaksLocalStorage);
+
+  input.addEventListener("keypress", () => {
+    input.style.width = `${input.value.length * 9}px`;
+  });
 
   input.addEventListener("keyup", () => {
     const task = new getTaskUseCase(TaksLocalStorage).execute(taskId);
